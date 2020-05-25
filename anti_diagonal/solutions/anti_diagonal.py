1a632c60dddaecc79ade7ab679a474c5fd8a4cbd
rsyakovlev <65895230+rsyakovlev@users.noreply.github.com>
2020-05-25 12:34:57 +0300

    Update anti_diagonal.py

--- anti_diagonal/solutions/anti_diagonal.py
+++ anti_diagonal/solutions/anti_diagonal.py
@@ -2,5 +2,4 @@ import numpy as np
 
 def anti_diagonal(a: np.ndarray) -> int:
     a1 = a[:, ::-1].diagonal()
-    a2 = a1[a1 > 0].mean()
-    return a[a > a2].size
+    return a[a > a1[a1 > 0].mean()].size
